aa640e2292232fb6201b553e00e98777165f89a9
Estela Valera <estela.valera96@gmail.com>
2024-10-17 12:10:26 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 
-function App() {
- //Aquí tu código
+const App = () => {
+    const [name, setName] = useState('Sofía'); 
+    const [newName, setNewName] = useState(''); 
+
+    const changeName = (e) => {
+        e.preventDefault(); 
+
+        if (newName.trim()) {
+        setName(newName); 
+        setNewName('');   
+    }
+};
+
+    return (
+        <div>
+            {}
+            <h2>Teacher Name: {name}</h2>
+        
+            {}
+            <form onSubmit={changeName}>
+            <input
+            type="text"
+            value={newName} 
+            onChange={(e) => setNewName(e.target.value)} 
+            placeholder="add a name"
+            />
+            <button type="submit">Add</button>
+            </form>
+        </div>
+    );
 }
 
-export default App
+export default App;
